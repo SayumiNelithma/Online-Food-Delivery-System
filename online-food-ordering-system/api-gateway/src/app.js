86873bcc54721cddb67dead5449c78027284165f
hirunikodisinghe <hirunikodisinghe@gmail.com>
2026-03-26 22:36:59 +0530

    continue with api gateway

--- online-food-ordering-system/api-gateway/src/app.js
+++ online-food-ordering-system/api-gateway/src/app.js
@@ -2,13 +2,33 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const { createProxyMiddleware } = require("http-proxy-middleware");
+
 require("dotenv").config();
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 app.use(morgan("dev"));
 
+// health check
+app.get("/", (req, res) => {
+    res.send("API Gateway Running");
+});
+
+// customer-service
+app.use(
+    "/api/customers",
+    createProxyMiddleware({
+        target: process.env.CUSTOMER_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+            "^/api/customers": "/customers"
+        }
+    })
+);
+
+// restaurant-service
 app.use(
     "/api/restaurants",
     createProxyMiddleware({
@@ -20,8 +40,52 @@ app.use(
     })
 );
 
-app.get("/", (req, res) => {
-    res.send("API Gateway Running");
-});
+// menu-service
+app.use(
+    "/api/menus",
+    createProxyMiddleware({
+        target: process.env.MENU_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+            "^/api/menus": "/menus"
+        }
+    })
+);
+
+// order-service
+app.use(
+    "/api/orders",
+    createProxyMiddleware({
+        target: process.env.ORDER_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+            "^/api/orders": "/orders"
+        }
+    })
+);
+
+// payment-service
+app.use(
+    "/api/payments",
+    createProxyMiddleware({
+        target: process.env.PAYMENT_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+            "^/api/payments": "/payments"
+        }
+    })
+);
+
+// delivery-service
+app.use(
+    "/api/deliveries",
+    createProxyMiddleware({
+        target: process.env.DELIVERY_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+            "^/api/deliveries": "/deliveries"
+        }
+    })
+);
 
 module.exports = app;
